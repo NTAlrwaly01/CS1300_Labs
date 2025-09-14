7962aee211c0e7bde745b955fff6bc2f6a68cf7e
Nawaf T Alrwaly <NTAlrwaly01@indianatech.net>
2025-09-14 13:28:48 +0300

    exercise1

---
+++ CS1300_Labs/lab4_1_strings.py
@@ -0,0 +1,33 @@
+# lab4_1_strings.py
+# Week 4 Session 1 Lab
+# Nawaf T Alrwaly
+# 2025-09-08
+
+print("=" * 50)
+print("WEEK 4 SESSION 1 LAB - STRING MANIPULATION")
+print("=" * 50)
+
+# ----------------- Exercise 1.1: Creating Strings -----------------
+# Different ways to create strings
+string1 = 'Hello'
+string2 = "World"
+string3 = '''This is
+a multiline
+string'''
+
+print("String 1:", string1)
+print("String 2:", string2)
+print("String 3:", string3)
+
+# YOUR TURN: Create these strings
+# String with apostrophe
+my_string1 = "It's a nice day"
+print("My string 1:", my_string1)
+
+# String with quotes
+my_string2 = 'He said "Hello"'
+print("My string 2:", my_string2)
+
+# My name
+my_name = "Nawaf T Alrwaly"
+print("My name:", my_name)
